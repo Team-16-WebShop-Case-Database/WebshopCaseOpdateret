8f37e41c27e49687ff8b9bb5419838ef4ab1bfb0
Serhat <seab63234@edu.ucl.dk>
2024-09-11 12:38:22 +0200

    Tilføjet OrderProduct klassen

---
+++ Model/OrderProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopCase.Model
+{
+    public class OrderProduct
+    {
+        public int OrderID { get; set; }
+        public Order Order { get; set; }
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public OrderProduct(int orderID, Order order, int productID, Product product, int quantity, decimal price)
+        {
+            OrderID = orderID;
+            Order = order;
+            ProductID = productID;
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
