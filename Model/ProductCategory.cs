919329be2fae9f3c7d8eb7d4ad37787fe63e8dcc
Serhat <seab63234@edu.ucl.dk>
2024-09-11 14:22:37 +0200

    Opdateret og tilføjet ProductCategory

---
+++ Model/ProductCategory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopCase.Model
+{
+    public class ProductCategory
+    {
+        public int ProductID { get; set; }  // Fremmednøgle (FK) til Product
+        public int CategoryID { get; set; } // Fremmednøgle (FK) til Category
+
+        // Navigations-egenskaber for relationerne
+        public Product Product { get; set; }  // Refererer til Product-objektet
+        public Category Category { get; set; } // Refererer til Category-objektet
+
+        // Overload-konstruktør for at initialisere link-tabellen
+        public ProductCategory(int productId, int categoryId)
+        {
+            ProductID = productId;
+            CategoryID = categoryId;
+        }
+
+        // Standardkonstruktør
+        public ProductCategory()
+        {
+        }
+    }
+}
